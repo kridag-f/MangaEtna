d18010e738f3520214b75273ef473b259d44b6d0
Tiity <anthony.cadet.94200@gmail.com>
2016-04-28 18:31:04 +0200

    zerety

--- JsonProcess.java
+++ JsonProcess.java
@@ -1,8 +1,10 @@
 package com.etna.tiitytheboss.tic_mobi;
 
+import android.provider.Settings;
 import android.widget.ListView;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Created by TiityTheBoSs on 28/04/2016.
@@ -21,6 +23,7 @@ public class JsonProcess {
     public void retrieveJson(String choiceSite, String completChoice, Integer choiceRequest, Integer itemChoice) {
 
         JSONArray jsonData = null;
+        JSONObject chapterJsonData = null;
         String fin = null;
 
         getApi.setChoiceSite(choiceSite);
@@ -29,18 +32,27 @@ public class JsonProcess {
 
         try {
             fin = getApi.execute().get();
-            jsonData = new JSONArray(fin);
-
-            setItemList(new String[jsonData.length()]);
-            String[] tmp = new String[jsonData.length()];
 
             if (choiceRequest < 3) {
+
+                jsonData = new JSONArray(fin);
+                setItemList(new String[jsonData.length()]);
+                String[] tmp = new String[jsonData.length()];
+
                 for(Integer i = 0; i < jsonData.length(); i++) {
                     tmp[i] = jsonData.getJSONObject(i).getString((String) getApi.getGetItemChoice().get(itemChoice));
                 }
                 setItemList(tmp);
             } else {
-                // Faire le tableau de string pour les chapitres vu que l'api return un JSONArray
+                chapterJsonData = new JSONObject(fin);
+                String[] tmp = new String[chapterJsonData.getJSONArray("chapters").length()];
+
+                System.out.print(chapterJsonData.getJSONArray("chapters").getJSONObject(0));
+
+                for(Integer j = 0; j < chapterJsonData.getJSONArray("chapters").length(); j++) {
+                    tmp[j] = chapterJsonData.getJSONArray("chapters").getJSONObject(j).getString((String) getApi.getGetItemChoice().get(itemChoice));
+                }
+                setItemList(tmp);
             }
 
             // System.out.println(jsonData.getJSONObject(0).getString("genreId"));
